09e1b344206bebcd9bb50b3e711ba8c1eb968c87
Shawn Allen <shawn.allen@gsa.gov>
2015-02-13 11:52:18 -0800

    jshint-certified

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 module.exports = function asyncdammit(sync) {
-  return function() {
+  'use strict';
+  return function async() {
     var args = [].slice.call(arguments),
         next = args.pop(),
-        nextType = typeof next;
+        nextType = typeof next,
+        value;
     if (nextType !== 'function') {
       throw new Error('expected callback function as last argument, got: ' + nextType);
     }
     try {
-      var value = sync.apply(this, args);
+      value = sync.apply(this, args);
     } catch (err) {
       return next(err);
     }
